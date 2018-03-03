ee09adff26d793196ec8749ed6c4d8837bc8dcc4
cwy007 <chanweiyan007@gmail.com>
2018-03-03 17:26:50 +0800

    Define the method

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -6,6 +6,10 @@ class Bottles
     "#{second_leading_text(number)} and pass it around, #{bottles_text(number - 1)} of beer on the wall.\n"
   end
 
+  def verses(from, to)
+    #code
+  end
+
   def zero_verse
     "No more bottles of beer on the wall, No more bottles of beer.\n" \
     "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
