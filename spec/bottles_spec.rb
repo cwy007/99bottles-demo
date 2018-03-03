af87689a55db4a42f728d23bc65ec8b3e1746578
cwy007 <chanweiyan007@gmail.com>
2018-03-03 17:32:05 +0800

    Add few verses (2 to 0)

--- spec/bottles_spec.rb
+++ spec/bottles_spec.rb
@@ -69,5 +69,20 @@ Take one down and pass it around, 97 bottles of beer on the wall.
         expect(Bottles.new.verses(99, 98)).to eq(expected)
       end
     end
+
+    context "2 to 0" do
+      it "returns correct verses" do
+        expected = <<-VERSE
+2 bottles of beer on the wall, 2 bottles of beer.
+Take one down and pass it around, 1 bottle of beer on the wall.
+1 bottle of beer on the wall, 1 bottle of beer.
+Take it down and pass it around, no more bottles of beer on the wall.
+No more bottles of beer on the wall, No more bottles of beer.
+Go to the store and buy some more, 99 bottles of beer on the wall.
+        VERSE
+
+        expect(Bottles.new.verses(2, 0)).to eq(expected)
+      end
+    end
   end
 end
